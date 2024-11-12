82482d5901ba58168a176da26734aab8cbc21086
MohammadReza Tayyebi <tayyebi_mohammadreza@hotmail.com>
2024-11-12 03:49:46 +0330

    replaced `adduser` command

--- start_vsftpd.sh
+++ start_vsftpd.sh
@@ -1,32 +1,20 @@
 #!/bin/sh
 
-#Remove all ftp users
+# Remove all ftp users
 grep '/ftp/' /etc/passwd | cut -d':' -f1 | xargs -r -n1 deluser
 
-#Create users
-#USERS='name1|password1|[folder1][|uid1][|gid1] name2|password2|[folder2][|uid2][|gid2]'
-#may be:
-# user|password foo|bar|/home/foo
-#OR
-# user|password|/home/user/dir|10000
-#OR
-# user|password|/home/user/dir|10000|10000
-#OR
-# user|password||10000|82
-
-#Default user 'ftp' with password 'alpineftp'
-
+# Default user 'ftp' with password 'alpineftp'
 if [ -z "$USERS" ]; then
   USERS="alpineftp|alpineftp"
 fi
 
-for i in $USERS ; do
+# Create users
+for i in $USERS; do
   NAME=$(echo $i | cut -d'|' -f1)
   GROUP=$NAME
   PASS=$(echo $i | cut -d'|' -f2)
   FOLDER=$(echo $i | cut -d'|' -f3)
   UID=$(echo $i | cut -d'|' -f4)
-  # Add group handling
   GID=$(echo $i | cut -d'|' -f5)
 
   if [ -z "$FOLDER" ]; then
@@ -38,24 +26,25 @@ for i in $USERS ; do
     if [ -z "$GID" ]; then
       GID=$UID
     fi
-    #Check if the group with the same ID already exists
+    # Check if the group with the same ID already exists
     GROUP=$(getent group $GID | cut -d: -f1)
     if [ ! -z "$GROUP" ]; then
-      GROUP_OPT="-G $GROUP"
+      GROUP_OPT="-g $GROUP"
     elif [ ! -z "$GID" ]; then
-      # Group don't exist but GID supplied
-      addgroup -g $GID $NAME
-      GROUP_OPT="-G $NAME"
+      # Group doesn't exist but GID supplied
+      groupadd -g $GID $NAME
+      GROUP_OPT="-g $NAME"
     fi
   fi
 
-  echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /sbin/nologin $UID_OPT $GROUP_OPT $NAME
+  useradd -d $FOLDER -s /sbin/nologin $UID_OPT $GROUP_OPT $NAME
+  echo "$NAME:$PASS" | chpasswd
   mkdir -p $FOLDER
   chown $NAME:$GROUP $FOLDER
   unset NAME PASS FOLDER UID GID
 done
 
-
+# Set passive mode port range
 if [ -z "$MIN_PORT" ]; then
   MIN_PORT=21000
 fi
@@ -64,10 +53,12 @@ if [ -z "$MAX_PORT" ]; then
   MAX_PORT=21010
 fi
 
+# Set passive mode address
 if [ ! -z "$ADDRESS" ]; then
   ADDR_OPT="-opasv_address=$ADDRESS"
 fi
 
+# Set TLS options
 if [ ! -z "$TLS_CERT" ] || [ ! -z "$TLS_KEY" ]; then
   TLS_OPT="-orsa_cert_file=$TLS_CERT -orsa_private_key_file=$TLS_KEY -ossl_enable=YES -oallow_anon_ssl=NO -oforce_local_data_ssl=YES -oforce_local_logins_ssl=YES -ossl_tlsv1=NO -ossl_sslv2=NO -ossl_sslv3=NO -ossl_ciphers=HIGH"
 fi
